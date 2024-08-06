86e4fcf5ee9548d07cb0f41e5b5015e300f6e565
kiruthikaharini <kiruthiramesh2106@gmail.com>
2024-08-06 16:41:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://userOne:zXnHSFN9TQ9BYTPy@todoapp.pw6ihnv.mongodb.net/?retryWrites=true&w=majority&appName=todoApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const taskSchema = new mongoose.Schema({
+    name: String
+});
+
+const Task = mongoose.model('task', taskSchema);
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// Home route to list tasks
+app.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.render('index', { tasks });
+});
+
+// Add task route
+app.post('/add', async (req, res) => {
+    const taskName = req.body.name;
+    if (taskName) {
+        await Task.create({ name: taskName });
+    }
+    res.redirect('/');
+});
+
+// Delete task route
+app.post('/delete', async (req, res) => {
+    const taskId = req.body.taskId;
+    if (taskId) {
+        await Task.findByIdAndDelete(taskId);
+    }
+    res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
